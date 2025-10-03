b2120fdcdc53320718f56d65bc5a780824d42de2
wndnjs00 <juwonjeon852@gmail.com>
2025-10-03 19:10:05 +0900

    다이얼로그

--- part1-chapter4-8/app/src/main/java/kr/co/fastcampus/part1/chapter4_8/MainActivity.kt
+++ part1-chapter4-8/app/src/main/java/kr/co/fastcampus/part1/chapter4_8/MainActivity.kt
@@ -34,25 +34,42 @@ fun DialogEx() {
     var counter by remember { mutableStateOf(0) }
 
     Column {
-        Button(onClick = { openDialog = !openDialog }) {
+        Button(onClick = { openDialog = true }) {
             Text("다이얼로그 열기")
         }
         Text("카운터: $counter")
     }
 
     if (openDialog) {
-        AlertDialog(onDismissRequest = {
-            // 단계 1: `openDialog`를 이용해 다이얼로그를 끌 수 있게 합니다.
-        }, confirmButton = {
-            // 단계 2: "더하기" 버튼을 만들고 `counter`를 증가시킵니다.
-            // 다이얼로그도 끕니다.
-        }, dismissButton = {
-            // 단계 3: "취소" 버튼을 만들고 다이얼로그를 끕니다.
-        }, title = {
-            // 단계 4: 타이틀을 만듭니다. "더하기" 정도로 해봅시다.
-        }, text = {
-            // 단계 5: 다이얼로그에서 설명할 문구를 출력합니다.
-        })
+        AlertDialog(
+            onDismissRequest = {
+                // 단계 1: `openDialog`를 이용해 다이얼로그를 끌 수 있게 합니다. -> 밖을 클릭했을땐 다이얼로그 종료하도록
+                openDialog = false
+            }, confirmButton = {
+                // 단계 2: "더하기" 버튼을 만들고 `counter`를 증가시킵니다.
+                // 다이얼로그도 끕니다.
+                Button(
+                    onClick = {
+                        counter++
+                        openDialog = false
+                    }) {
+                    Text("더하기")
+                }
+            }, dismissButton = {
+                // 단계 3: "취소" 버튼을 만들고 다이얼로그를 끕니다.
+                Button(
+                    onClick = {
+                        openDialog = false
+                    }) {
+                    Text("취소")
+                }
+            }, title = {
+                // 단계 4: 타이틀을 만듭니다. "더하기" 정도로 해봅시다.
+                Text("더하기")
+            }, text = {
+                // 단계 5: 다이얼로그에서 설명할 문구를 출력합니다.
+                Text("더하기 버튼을 누르면\n카운더가 증가됩니다.")
+            })
     }
 }
 
