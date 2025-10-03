0f3e007873a1312a8cd26034a7f5a35198646272
wndnjs00 <juwonjeon852@gmail.com>
2025-10-03 18:39:18 +0900

    Compose Canvas 활용

--- part1-chapter4-7/app/src/main/java/kr/co/fastcampus/part1/chapter4_7/MainActivity.kt
+++ part1-chapter4-7/app/src/main/java/kr/co/fastcampus/part1/chapter4_7/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,8 +39,11 @@ fun CanvasEx() {
 
         // 단계 1: `drawLine`을 사용해봅시다. 파라미터로 색상, 시작(`Offset`)
         // 끝(`Offset` 타입)을 받습니다.
+        drawLine(Color.Red, Offset(20f,10f), Offset(50f,30f))
 
         // 단계 2: `drawCircle`을 사용해보세요. 색상, 반지름, 중앙(`Offset`)
+        drawCircle(Color.Yellow, 10f, Offset(15f,40f))
+        drawRect(Color.Magenta, Offset(30f,30f), Size(10f,10f))
 
         // 단계 3: 아래의 규칙으로 그려진 아이콘 `Icons.Filled.Send`를
         // `drawLine`으로 변경해봅시다.
@@ -52,9 +56,16 @@ fun CanvasEx() {
         //        lineTo(23.0f, 12.0f)
         //        lineTo(2.01f, 3.0f)
         //        lineTo(2.0f, 10.0f)
-        //        lineToRelative(15.0f, 2.0f)
-        //        lineToRelative(-15.0f, 2.0f)
-        //        close()
+        //        lineToRelative(15.0f, 2.0f) //더하기
+        //        lineToRelative(-15.0f, 2.0f) // 더하기
+        //        close() // 종료
+
+        drawLine(Color.Green, Offset(2.01f, 21.0f), Offset(23.0f, 12.0f))
+        drawLine(Color.Green, Offset(23.0f, 12.0f), Offset(2.01f, 3.0f))
+        drawLine(Color.Green, Offset(2.01f, 3.0f), Offset(2.0f, 10.0f))
+        drawLine(Color.Green, Offset(2.0f, 10.0f), Offset(17.0f, 12.0f))
+        drawLine(Color.Green, Offset(17.0f, 12.0f), Offset(2.0f, 14.0f))
+        drawLine(Color.Green, Offset(2.0f, 14.0f), Offset(2.01f, 21.0f))
     }
 }
 
